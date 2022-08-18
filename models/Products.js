fe65dee2caf092d025e9c4792f5cfc8fb80f2cbb
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2022-08-18 12:19:28 -0300

    corrigindo o model de produtos

--- models/Products.js
+++ models/Products.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const Products = mongoose.model('Products', {
     name: String,
     description: String,
+    amount: Number,
     brand: String,
     purchasePrice: Number,
     saleValue: Number,
