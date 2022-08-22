1682d6b0ab948a4d537171a0df3f8dfe57b8cfc7
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2022-08-22 12:09:20 -0300

    Adicionando rota para atualizar e deletar produto

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -53,6 +53,50 @@ router.get('/:id', async (req, res) => {
     }
 })
 
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id
+    const { name, description, amount, brand, purchasePrice, saleValue} = req.body
+
+    const products = {
+        name,
+        description,
+        amount,
+        brand,
+        purchasePrice,
+        saleValue
+    }
+    
+    try {
+        const updateProduct = await Products.updateOne({_id: id}, products)
+
+        if(updateProduct.matchedCount === 0) {
+            res.status(422).json({message: 'O produto não foi encontrado'})
+            return
+        }
+
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    const id = req.params.id
+    
+    const products = await Products.findOne({_id: id})
+    if(!products){
+        res.status(422).json({message: 'Produto não encontrado!'})
+        return
+    }
+
+    try {
+        await Products.deleteOne({_id: id})
+        res.status(200).json({message: 'Produto deletado com sucesso!'})
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
 
 
 module.exports = router
