0e171bd6781fcbab5dd32f718ad6d489f550c639
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2022-09-09 16:21:05 -0300

    Alterando o model de users para typescript

---
+++ models/Users.ts
@@ -0,0 +1,32 @@
+import { Table, Column, Model, DataType, AllowNull } from 'sequelize-typescript'
+import { Optional } from 'sequelize'
+
+
+interface UsersAttributes {
+    id: number
+    name: string
+    email: string,
+    password: string
+}
+
+interface UsersCreationAttributes extends Optional<UsersAttributes, 'id'> { }
+
+@Table({
+    timestamps: true
+})
+
+class Users extends Model<UsersAttributes, UsersCreationAttributes> {
+
+    @Column(DataType.TEXT)
+    name: string | undefined
+
+    @Column(DataType.TEXT)
+    email: string | undefined
+
+    @Column(DataType.TEXT)
+    password: string | undefined
+
+}
+
+export { Users }
+
